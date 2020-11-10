bb4cde09eff6305f73f13dc16c39552dbf0a7229
morenoloza98 <morenolozalex98@gmail.com>
2020-11-10 13:21:26 -0600

    finished MMs basic backend

--- app/src/core/classes/MMs.ts
+++ app/src/core/classes/MMs.ts
@@ -1,6 +1,6 @@
 import IModel from "../interfaces/IModel";
 import Model from "./Model";
-import factorial from "../utils"
+import { factorial, sum } from "../utils"
 
 class MMs extends Model implements IModel{
     lq:number
@@ -26,7 +26,26 @@ class MMs extends Model implements IModel{
         this.ro = this.lambda / (this.s * this.miu)
         this.lq = (this.p0 * ((this.lambda / this.miu) ** this.s) * this.ro) / (factorial(this.s) * ((1 - this.ro) ** 2))
         this.l = this.lq + (this.lambda / this.miu)
+        const result = sum(this.s, this.lambda, this.miu)
+        this.p0 = 1 / (result + ((((this.lambda / this.miu) ** this.s) / factorial(this.s)) * (1 / (1 - (this.lambda / (this.s * this.miu))))))
         return 0
+    }
+
+    getWq(t:number):number {
+        if(t >= 0) {
+            const e = Math.E ** (-this.s * this.miu * t * (1 - this.ro))
+            this.wq = ((((this.s * this.ro) ** this.s) * this.p0) / (factorial(this.s) * (1 - this.ro))) * e
+            return this.wq
+        } else {
+            return -1
+        }
+    }
+
+    getW(t:number):number {
+        const e = Math.E ** (-this.miu * t)
+        const e2 = Math.E ** (-this.miu * t * (this.s - 1 - this.s * this.ro))
+        this.w = e * (1 + (((((this.s * this.ro) ** this.s) * this.p0) * (1 - e2)) / (factorial(this.s) * (1- this.ro) * (this.s - 1 - (this.s * this.ro)))))
+        return this.w
     }
 
     getPn(n:number):number {
--- app/src/core/utils.ts
+++ app/src/core/utils.ts
@@ -1,4 +1,4 @@
-export default function factorial(num:number):number{
+export function factorial(num:number):number {
     if(num < 0) {
         return -1
     } else if(num == 0)
@@ -6,4 +6,12 @@ export default function factorial(num:number):number{
     else {
         return (num * factorial(num - 1))
     }
+}
+
+export function sum(s:number, lambda:number, miu:number):number {
+    let result:number = 0
+    for (let n = 0; n = s - 1; n++) {
+        result += (((lambda / miu) ** n) / factorial(n))
+    }
+    return result
 }
